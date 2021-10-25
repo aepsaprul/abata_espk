e28eb52bc504c61ea548c9dc0d6af918a4b7bcbb
aep <aepsaprul18@gmail.com>
2021-10-25 10:44:47 +0700

    fix error BM tidak bisa ubah status ketika status "menunggu disetujui"

--- resources/views/pages/pekerjaan/proses_pekerjaan/index.blade.php
+++ resources/views/pages/pekerjaan/proses_pekerjaan/index.blade.php
@@ -129,7 +129,7 @@
                                 <td>
                                     @if ($pesanan->status_id != null)
                                         {{ $pesanan->status->nama_status }}
-                                        @if ($pesanan->status_id == 6 || $pesanan->status_id == 7)
+                                        @if ($pesanan->status_id != null && $pesanan->status_id != 9)
                                             @php $hide = "d-none"; @endphp
                                         @else
                                             @php $hide = ""; @endphp
